adea2ff0fbaded954155b85daa0ac71943e825a5
Sydorenko Vladyslav <cayceworld@outlook.com>
2022-05-25 14:48:58 +0100

    Add tests for ResultBox component

--- src/components/ResultBox/ResultBox.js
+++ src/components/ResultBox/ResultBox.js
@@ -8,15 +8,17 @@ import styles from './ResultBox.module.scss';
 const ResultBox = ({ from, to, amount }) => {
 
   const convertedAmount = useMemo(() => {
-    if(from === 'USD' && to === 'PLN') return convertUSDToPLN(amount);
-    if(from === 'PLN' && to === 'USD') return convertPLNToUSD(amount);
+    if (amount < 0) return ('Wrong value');
+    if (from === 'USD' && to === 'PLN') return convertUSDToPLN(amount);
+    if (from === 'PLN' && to === 'USD') return convertPLNToUSD(amount);
     return formatAmountInCurrency(amount, from);
   }, [from, to, amount]);
 
+  console.log(amount);
   const formattedAmount = useMemo(() => formatAmountInCurrency(amount, from), [amount, from]);
 
   return (
-    <div className={styles.result}>
+    <div className={styles.result} data-testid="result-box">
       {formattedAmount} = {convertedAmount}
     </div>
   );
